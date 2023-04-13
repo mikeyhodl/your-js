26b898b20c89780f81eee59905d68d6a2660b6ae
Mike Owino <mike.owino40@yahoo.com>
2023-04-13 07:27:42 +0000

    js

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -2,11 +2,6 @@ describe('empty spec', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
-  it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
-  })
   it('renders the image', () => {
     cy.get('img')
     .should('be.visible')
